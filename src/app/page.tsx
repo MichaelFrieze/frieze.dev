c4ace8a89c34d0452b2025035c0eb47ad3834fa6
Michael Frieze <michaelfrieze@icloud.com>
2024-03-25 21:23:38 -0400

    change: update Home page body text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,30 +68,16 @@ export default function Home() {
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              My journey into web development began with a spark of curiosity at
+              25, leading me on a path of continuous learning and growth. I went
+              to Oakland University and landed a job there working on the
+              student portal as a Java Developer.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
-            </p>
-            <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              Having transitioned from Java to front-end development, I now
+              focus on leveraging React and cutting-edge technologies to design
+              intuitive interfaces and dynamic web solutions that stand out in
+              today&apos;s digital landscape.
             </p>
           </div>
         </div>
