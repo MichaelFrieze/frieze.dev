082a36dd01cb8187232e265aa0737352f0dc5337
Michael Frieze <michaelfrieze@icloud.com>
2025-09-08 13:16:16 -0400

    feat: improve search bar and search card

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -1,6 +1,9 @@
 import type { CollectionEntry } from 'astro:content'
 import { Separator } from './ui/separator'
 import { Badge } from './ui/badge'
+import { Card } from './ui/card'
+import { Hash } from 'lucide-react'
+import { Image as UnpicImage } from '@unpic/react'
 import { formatDate } from '@/lib/utils'
 
 export interface Props {
@@ -14,25 +17,45 @@ export default function SearchCard({ href, frontmatter }: Props) {
 
   return (
     <li className="my-6">
-      <div className="not-prose hover:bg-border/40 rounded-xl border p-4 transition-colors duration-300 ease-in-out">
+      <Card className="bg-card hover:bg-muted px-4 py-4 transition-colors duration-300 ease-in-out">
         <a href={href} className="flex flex-col gap-4 sm:flex-row">
+          {frontmatter.image && (
+            <div className="md:max-w-3xs md:shrink-0">
+              <UnpicImage
+                src={
+                  (frontmatter.image as any).src ?? String(frontmatter.image)
+                }
+                alt={title}
+                width={1200}
+                height={630}
+                className="rounded-xl object-cover"
+              />
+            </div>
+          )}
           <div className="flex-grow">
-            <h3 className="mb-1 text-lg font-semibold">{title}</h3>
-            <p className="text-muted-foreground mb-2 text-sm">{description}</p>
-            <div className="text-muted-foreground mb-2 flex flex-wrap items-center gap-x-2 text-xs">
+            <h3 className="mb-1 text-lg font-medium">{title}</h3>
+            <p className="text-foreground/80 mb-2 text-sm">{description}</p>
+            <div className="text-foreground/80 mb-2 flex flex-wrap items-center gap-x-2 text-xs">
               <span>{formattedDate}</span>
               <Separator orientation="vertical" className="h-4" />
             </div>
             {tags && (
               <div className="flex flex-wrap gap-2">
                 {tags.map((tag) => (
-                  <Badge variant="secondary">{tag}</Badge>
+                  <Badge
+                    key={tag}
+                    variant="default"
+                    className="flex items-center gap-x-1"
+                  >
+                    <Hash className="size-3" />
+                    {tag}
+                  </Badge>
                 ))}
               </div>
             )}
           </div>
         </a>
-      </div>
+      </Card>
     </li>
   )
 }
